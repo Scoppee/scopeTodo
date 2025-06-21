378239e251c469a4f82d28ebb7b0193ec8575d0b
Scoppee <bhorluscope314@gmail.com>
2025-06-21 07:09:45 +0100

    Modify css and javascript links in the pages folder.

--- script/script.js
+++ script/script.js
@@ -26,3 +26,49 @@ openDialog.addEventListener("click", () => {
 closeDialog.addEventListener("click", () => {
     dialog.close();
 })
+
+
+
+// Single Application Scripts
+// Create a map that direct user to pages
+const routes = {
+    "/": "/pages/home.html",
+    "/tasks": "/pages/tasks.html",
+    "/projects": "/pages/projects.html",
+    "/pomodoro": "/pages/pomodoro.html",
+    "/eisenhowerMatrix": "/pages/eisenhower-matrix.html",
+    "/singleProject": "/pages/single-project.html",
+    "taskBoard": "/pages/task-board.html"
+}
+
+// Change the UL in the browser
+const navigateTo = (url) => {
+    history.pushState(null,null,url);
+    renderPage();
+}
+
+
+// Get the current path from url, fetch the right HTML file based on the routes map
+// Injects that HTML into the <main id="app"> container
+const renderPage = async () => {
+    const path = location.pathname
+    const html = await fetch(routes[path] || routes["/"]).then((res) => res.text());
+    document.querySelector("#app").innerHTML = html;
+}
+
+
+
+// Popstate: Ensure the back/forward buttons of the browser works
+window.addEventListener("popstate",  renderPage)
+
+// Intercepting Link clicks: when a link with data link is clicked, it doesnt reload thenpage
+// Instead, it navigates using the navigateTo function
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", (e) => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.getAttribute("href"));
+        }
+    })
+    renderPage();
+})
